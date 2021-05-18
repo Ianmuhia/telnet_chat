c9e41e1616e29f8053ada4760ee7f68cdb43a608
ian <muhia>
2021-05-18 16:18:28 +0300

    final changes

--- main.go
+++ main.go
@@ -7,11 +7,23 @@ import (
 
 func main() {
 	s := newServer()
+
+	go s.run()
 	listener, err := net.Listen("tcp", "8888")
 	if err != nil {
-		log.Fatal("unable to start server")
+		log.Fatalf("unable to start server : %s", err.Error())
 	}
 
 	defer listener.Close()
-	log.Println("started server on :8888")
+	log.Printf("started server on :8888")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Unable to accept connection, %s", err.Error())
+			continue
+
+		}
+		go s.newClient(conn)
+	}
 }
